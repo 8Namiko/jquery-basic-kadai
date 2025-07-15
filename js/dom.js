3439eb0a46c797df9852804fb6aea16eb70c84eb
8Namiko <guineapig.8735@gmail.com>
2025-07-15 10:29:28 +0900

    kadai008

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+$(function(){
+  let num = 0;
+
+  $('#append').on('click', function(){
+    num++;
+    $('ul').append('<li>appendで追加'+ num + '</li>');
+  });
+});
---
+++ kadai_008/form.js
@@ -0,0 +1,7 @@
+$(function () {
+
+$('.btn').on('click', function() {
+  let input = $('.text-box').val('クリックしました！');
+});
+
+});
